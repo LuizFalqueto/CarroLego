4a4ab835bf43d81bd9fd87331c34df11e05d26cb
Luiz Gustavo Falqueto <lgfalqueto@hotmail.com>
2019-06-28 10:20:47 -0300

    Add files via upload

---
+++ CarroLego/src/carro_lego/VeiculoSmart.java
@@ -0,0 +1,34 @@
+package carro_lego;
+
+public class VeiculoSmart extends Veiculo{
+	
+	
+	public void anda(){
+		
+	}
+	
+	public void segueLinha(){
+		
+	}
+	
+	public void segueLinhaAteBola(){
+		
+	}
+	
+	public void seguraBola(){
+		
+	}
+	
+	public void largaBola(){
+		
+	}
+	
+	public void pegaBolaNalinha(){
+		
+	}
+	
+	public void voltaAoPontoDeOrigem(){
+		
+	}
+	
+}
